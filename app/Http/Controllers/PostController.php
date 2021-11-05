6313deeedddecbc72413ff5a51fb637a2ce9d8ec
Antonio <antonioru1997@gmail.com>
2021-11-05 17:46:31 +0100

    FINE (aggiunto orderBy)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::orderBy('created_at', 'DESC')->get();
         // dump($posts);
         return view('guest.posts.index', compact('posts'));
     }
